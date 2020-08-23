5441f0b9b6db4fd15586a17529be2437d3299dfa
Masahiro Shindo <shindo@concentinc.jp>
2020-08-24 01:16:32 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# しいたけスクレイパー
+
+## これは
+「しいたけ占い」からサマリー部のテキストを抽出してJSONで返却
+
+## しいたけ占い
+https://voguegirl.jp/horoscope/shiitake/sagittarius/
+
+## アクセスURI
+
+### デフォルト（牡羊座）
+
+```
+https://shitakescraper.herokuapp.com/
+```
+
+### 8月1日(0801)生まれ = 獅子座
+
+```
+https://shitakescraper.herokuapp.com/?birthmmdd=0801
+```
+
+## レスポンス例
+
+```
+{
+  "shiitake": {
+    "titleText": "しいたけ占い",
+    "sourceLocation": "https://voguegirl.jp/horoscope/shiitake/leo/",
+    "headlineText": "獅子座",
+    "termText": "8月17日(月)~8月23日(日)の運勢は？",
+    "summaryText": "「いつの間にか始まっている」のオレンジ色が出ています。今週の獅子座は悪い意味ではなくて、自分自身がちょっとぼーっとしている間に、「あ、結構大きな動きが始まっていっているぞ」と気づくようなことが多く出てきたりします。ただ、今週は「ぼーっとしていること」はよいことだと思ってほしいのです。というのは、獅子座は結構「第2スタートの人」であるからです。あなたは普段、ある程度は警戒心が強い人です。他人のこと、そして、自分の状況をよく調べてから物事をスタートしていくことが多いのですが、中には「よく考えないで返事をしたら始まった」とか、そういうこともある人なのです。むしろ、あなたにとって大事なことは「ノリ」でスタートすることが多い。そして、「あ、そうだ。私はこれをやんなきゃいけないんだったわ」と、少し遅れて自分の中で本格的にエンジンがかかるようなことも多いのです。"
+  }
+}
+```
+
+
+## 参考
+- https://jsprimer.net/use-case/nodecli/helloworld/
+- https://note.com/w0o0ps/n/n2eca493ced5d
+- https://qiita.com/kents1002/items/e1b90c71897c55b8e870
+- https://keruuweb.com/node-js%E3%82%A2%E3%83%97%E3%83%AA%E3%82%92heroku%E3%81%AB%E3%83%87%E3%83%97%E3%83%AD%E3%82%A4%E3%81%99%E3%82%8B/
+- https://qiita.com/ledsun/items/0965a60f9bdff04f2fa0
---
+++ parser.js
@@ -0,0 +1,162 @@
+// ===================================================== Data and Config
+const dataResource = {
+  // 星座リスト
+  'dateAndconstellation': {
+    '0120': 'aquarius',
+    '0219': 'pisces',
+
+    '0321': 'aries',
+    '0420': 'taurus',
+    '0521': 'gemini',
+    '0622': 'cancer',
+    '0723': 'leo',
+    '0823': 'virgo',
+    '0923': 'libra',
+    '1024': 'scorpio',
+    '1123': 'sagittarius',
+    '1222': 'capricorn',
+  }
+}
+
+const dataConfig = {
+  // 接続先：「しいたけ占い」
+  'siteInfo': {
+    'shitake': {
+      'domain': 'https://voguegirl.jp',
+      'path': '/horoscope/shiitake'
+    }
+  }
+}
+
+// ===================================================== External Module
+const request = require('request')
+const {
+  JSDOM
+} = require('jsdom')
+
+const url = require('url');
+
+
+// ===================================================== Main Routine
+//getConstellationFromDate('1123')
+//requestSequence()
+listenHTTP()
+
+
+// ===================================================== Sub Routines
+
+// mmdd(ex. 0109) をキーに星座名を返却
+function getConstellationFromDate(mmddSrc) {
+
+  const seriesDateConstellation = Object.keys(dataResource.dateAndconstellation).sort()
+
+  const dateDefault = '0321'
+
+  // 入力値バリデート (mmdd)
+  const regex = /^[0-9]{4}$/
+  const mmdd = (function() {
+    if(!mmddSrc) { return dateDefault }
+    if(!(mmddSrc.match(regex) && mmddSrc.match(regex).length)) { return dateDefault }
+
+    const mmSrc = mmddSrc.substring(0, 2);
+    if(Number(mmSrc) > 12 || Number(mmSrc) < 1) { return dateDefault }
+
+    const ddSrc = mmddSrc.substring(2);
+    if(Number(ddSrc) > 31 || Number(ddSrc) < 1) { return dateDefault }
+
+    return mmddSrc
+  })()
+
+  let idConstellation
+  seriesDateConstellation.some((dateConstellation, ix) => {
+    if(mmdd < seriesDateConstellation[0]) {
+      idConstellation = dataResource['dateAndconstellation'][seriesDateConstellation[0]]
+      return true;
+    }
+    if(mmdd < dateConstellation) {
+      idConstellation = dataResource['dateAndconstellation'][seriesDateConstellation[ix -1]]
+      return true
+    }
+    if(mmdd >= seriesDateConstellation[seriesDateConstellation.length - 1]) {
+      idConstellation = dataResource['dateAndconstellation'][seriesDateConstellation[seriesDateConstellation.length - 1]]
+      return true;
+    }
+  })
+
+  //console.log(ret)
+  return idConstellation
+}
+
+// スクレイピング元サイトにアクセスし、スクレイピング結果を返す
+function httpRequestSequence(nameConstellation) {
+  const dataResponse = {
+    "shiitake": {
+      "summaryText": ""
+    }
+  }
+
+  //console.log(nameConstellation)
+
+  const siteInfo = dataConfig['siteInfo']['shitake']
+  const sourceLocation = siteInfo['domain'] + siteInfo['path'] + '/' + nameConstellation + '/'
+
+  return new Promise((resolve, reject) => {
+    request(sourceLocation, (errorExternalAccess, response, body) => {
+      if (errorExternalAccess) {
+        console.error(errorExternalAccess)
+        reject(errorExternalAccess)
+      }
+
+      try {
+        const dom = new JSDOM(body, {url: siteInfo['domain']})
+
+        const domSelector = (selector) => dom.window.document.querySelector(selector);
+
+        dataResponse['shiitake'] = {
+          'titleText': domSelector('.o-shiitake-detail__header .a-headline').textContent.trim(),
+          'sourceLocation': siteInfo['domain'] + '/' + siteInfo['path'] + '/' + nameConstellation + '/',
+          'headlineText': domSelector('.o-shiitake-detail__title-texts .a-headline').textContent.trim(),
+          'termText': domSelector('.o-shiitake-detail__date').textContent.replace(/\s/g, '').trim(),
+          'summaryText': domSelector('.o-shiitake-detail__section .a-text').textContent.trim(),
+        }
+
+        //console.log(JSON.stringify(dataResponse))
+
+        // スクレイピング結果返却
+        resolve(dataResponse)
+      }
+      catch (errorInSequence) {
+        console.error(errorInSequence)
+        reject(errorInSequence)
+      }
+    })
+  })
+}
+
+
+function listenHTTP() {
+  const http = require('http');
+  const server = http.createServer(function (request, response) {
+
+    //console.log(JSON.stringify(request.url))
+
+    const url_parts = url.parse(request.url, true);
+    const query = url_parts.query;
+    const mmdd = query['birthmmdd']
+
+    const nameConstellation = getConstellationFromDate(mmdd)
+    //console.log('====', nameConstellation)
+
+    httpRequestSequence(nameConstellation).then((dataResponse) => {
+      response.writeHead(200, {'Content-Type': 'application/json'})
+      response.write(JSON.stringify(dataResponse, null , "  "))
+      response.end();
+    })
+    .catch(() => {
+      response.write('{}')
+      console.error('FAIL')
+    })
+  })
+
+  server.listen(process.env.PORT || 8080);
+}
